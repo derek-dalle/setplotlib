e5bf1a0528ab56f4c88d9c98b49446a3ce196d20
Derek Dalle <derek.dalle@gmail.com>
2013-03-03 12:28:52 -0500

    Capitulated to use tight_layout()... fails in non-interactive mode

--- src/setplotlib.py
+++ src/setplotlib.py
@@ -65,14 +65,9 @@ def set_plot(h_f=None):
 	# Get the axes.
 	h_a = h_f.get_axes();
 	# Check for evil subplots.
-	if len(h_a) > 1:
-		# Subplots detected!
-		raise ImportError("Subplots not supported.");
-	elif len(h_a) == 0:
+	if len(h_a) == 0:
 		# Empty figure.
 		raise ImportError("No axes to manipulate!");
-	# Extract the first axes.
-	h_a = h_a[0];
 	
 	# Now get the size of the figure.
 	L_x_i = h_f.get_figwidth()  + 0.0;
@@ -106,79 +101,37 @@ def set_plot(h_f=None):
 		# Really make it use Times New Roman.
 		h.set_family('Times New Roman');
 	
-	# Get handles for the tick marks.
-	h_t_x = h_a.get_xticklines();
-	h_t_y = h_a.get_yticklines();
-	# NOTE: This will be controlled by user options.
-	# Put the ticks in the correct location
-	matplotlib.pyplot.setp(h_t_x, 'marker', 3);
-	matplotlib.pyplot.setp(h_t_y, 'marker', 0);
-	# Make them smaller (or the right size, anyway).
-	matplotlib.pyplot.setp(h_t_x, 'markersize', 2);
-	matplotlib.pyplot.setp(h_t_y, 'markersize', 2);
-	# Delete the right and top box ("spines").
-	h_a.spines['right'].set_color('none');
-	h_a.spines['top'].set_color('none');
-	# Tone done the remaining splines.
-	h_a.spines['left'].set_lw(0.8);
-	h_a.spines['bottom'].set_lw(0.8);
-	# Turn off the extra ticks.
-	h_a.xaxis.set_ticks_position('bottom');
-	h_a.yaxis.set_ticks_position('left');
-	
-	# Calculate required margins (in inches).
-	# Check for interactive status.
-	if matplotlib.pyplot.isinteractive():
-		# Update the text box locations.
-		matplotlib.pyplot.draw();
-		# Initialize the coordinates for the containing box.
-		x_min = L_x*dpi_f+0.0; x_max = 0.0;
-		y_min = L_y*dpi_f+0.0; y_max = 0.0;
-		# Loop through text objs again to find limits.
-		for h in h_f.findobj(matplotlib.text.Text):
-			# Ignore empty strings.
-			if not (h._renderer is None) and h.get_text():
-				# Get the containing box for the object.
-				bb = h.get_window_extent();
-				# Compare limits to running values.
-				x_min = min(x_min, bb.xmin);
-				x_max = max(x_max, bb.xmax);
-				y_min = min(y_min, bb.ymin);
-				y_max = max(y_max, bb.ymax);
-		# Bounding box of the existing axes object.
-		bb_a_i = h_a.get_position();
-		# Calculate the minimum required margins to fit all text.
-		margin_l =  bb_a_i.xmin*L_x - x_min/dpi_f;
-		margin_r = -bb_a_i.xmax*L_x + x_max/dpi_f;
-		margin_b =  bb_a_i.ymin*L_y - y_min/dpi_f;
-		margin_t = -bb_a_i.ymax*L_y + y_max/dpi_f;
-		# Set buffers (in inches)
-		b_l = 2./72;
-		b_r = 2./72;
-		b_b = 2./72;
-		b_t = 2./72;
-		# Final axis location to contain text and buffers.
-		xmin_a = (margin_l + b_l) / L_x;
-		ymin_a = (margin_b + b_b) / L_y;
-		xmax_a = 1 - (margin_r + b_r) / L_x;
-		ymax_a = 1 - (margin_t + b_t) / L_y;
-		# Make a bounding box out of these parameters.
-		bb_a = matplotlib.transforms.Bbox(
-			array([[xmin_a, ymin_a], [xmax_a, ymax_a]]));
-		# Apply it!
-		h_a.set_position(bb_a);
-		
-	else:
-		# Just use the built-in tight_axis funciton
-		matplotlib.pyplot.tight_layout();
-	
-	
+	# Loop through all axes.
+	for h in h_a:
+		# Get handles for the tick marks.
+		h_t_x = h.get_xticklines();
+		h_t_y = h.get_yticklines();
+		# NOTE: This will be controlled by user options.
+		# Put the ticks in the correct location
+		matplotlib.pyplot.setp(h_t_x, 'marker', 3);
+		matplotlib.pyplot.setp(h_t_y, 'marker', 0);
+		# Make them smaller (or the right size, anyway).
+		matplotlib.pyplot.setp(h_t_x, 'markersize', 2);
+		matplotlib.pyplot.setp(h_t_y, 'markersize', 2);
+		# Delete the right and top box ("spines").
+		h.spines['right'].set_color('none');
+		h.spines['top'].set_color('none');
+		# Tone done the remaining splines.
+		h.spines['left'].set_lw(0.8);
+		h.spines['bottom'].set_lw(0.8);
+		# Turn off the extra ticks.
+		h.xaxis.set_ticks_position('bottom');
+		h.yaxis.set_ticks_position('left');
 	
 	# Final update.
-	matplotlib.pyplot.draw_if_interactive();
+	if matplotlib.pyplot.isinteractive():
+		matplotlib.pyplot.draw();
+	
+	# Ok, let's see if the tight_layout works.
+	h_f.tight_layout(pad=0.2);
 	
 	# Get the top-level line objects.
-	h_l = h_a.get_lines();
+	h_l = [h.get_lines() for h in h_a];
 	# Make a dictionary of the useful handles.
 	h = {
 		'figure': h_f,
